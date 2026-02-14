182d7a96cfda8863cdd3ad426865ab3f115393c0
Sanja Bhargav <sanja@example.com>
2026-02-14 11:45:33 +0530

    Remove router for SPA scrolling site

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
 
-const App = () => (
-  <Routes>
-    <Route path="/" element={<Index />} />
-    <Route path="*" element={<NotFound />} />
-  </Routes>
-);
+const App = () => <Index />;
 
 export default App;
+
+
+
+
+
+
+// import { Routes, Route } from "react-router-dom";
+// import Index from "./pages/Index";
+// import NotFound from "./pages/NotFound";
+
+// const App = () => (
+//   <Routes>
+//     <Route path="/" element={<Index />} />
+//     <Route path="*" element={<NotFound />} />
+//   </Routes>
+// );
+
+// export default App;
